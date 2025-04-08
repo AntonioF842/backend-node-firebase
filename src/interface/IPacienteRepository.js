044654ad3525a07e91828e311dc8db485e686c94
AntonioF842 <antoemman@hotmail.com>
2025-04-08 02:34:09 -0600

    Modificaciones para el funcionamiento del Frontend

---
+++ src/interface/IPacienteRepository.js
@@ -0,0 +1,25 @@
+export default class IPacienteRepository {
+    /*
+    Crear Paciente
+    @param {Objeto} = Datos del Paciente
+    @return {Promise<Objeto>} = Paciente Creado
+    */
+   create (paciente) {
+    throw new Error('Método implementado')
+   }
+   update (id, updateData) {
+    throw new Error('Método implementado')
+   }
+   delete (id) {
+    throw new Error('Método implementado')
+   }
+   getAll () {
+    throw new Error('Método implementado')
+   }
+   findByName (fullName) {
+    throw new Error('Método implementado')
+   }
+   getById(id) {
+    throw new Error('Método implemetado')
+   }
+}
